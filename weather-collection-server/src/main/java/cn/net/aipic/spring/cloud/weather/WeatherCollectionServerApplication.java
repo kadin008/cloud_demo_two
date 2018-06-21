fa27a25d04330c0f217ca2c1c78a996f1193ed8d
kadin008 <wangchengbin_2009@vip.163.com>
2018-06-21 22:10:02 +0800

    测试项目1

---
+++ weather-collection-server/src/main/java/cn/net/aipic/spring/cloud/weather/WeatherCollectionServerApplication.java
@@ -0,0 +1,12 @@
+package cn.net.aipic.spring.cloud.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherCollectionServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherCollectionServerApplication.class, args);
+    }
+}
