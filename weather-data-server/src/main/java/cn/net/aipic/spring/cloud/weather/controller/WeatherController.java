ad0c42e1d19a3afaecaea1e6cb8b79dc6cbc5249
kadin008 <wangchengbin_2009@vip.163.com>
2018-06-21 23:22:12 +0800

    更新

---
+++ weather-data-server/src/main/java/cn/net/aipic/spring/cloud/weather/controller/WeatherController.java
@@ -0,0 +1,30 @@
+package cn.net.aipic.spring.cloud.weather.controller;
+
+import cn.net.aipic.spring.cloud.weather.service.WeatherDataService;
+import cn.net.aipic.spring.cloud.weather.vo.WeatherResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 天气Controller
+ */
+@RestController
+@RequestMapping("/weather")
+public class WeatherController {
+
+    @Autowired
+    private WeatherDataService weatherDataService;
+
+    @GetMapping("/cityId/{cityId}")
+    public WeatherResponse getWeatherByCityId(@PathVariable("cityId") String cityId) {
+        return weatherDataService.getDataByCityId(cityId);
+    }
+
+    @GetMapping("/cityName/{cityName}")
+    public WeatherResponse getWeatherByCityName(@PathVariable("cityName") String cityName) {
+        return weatherDataService.getDataByCityName(cityName);
+    }
+}
