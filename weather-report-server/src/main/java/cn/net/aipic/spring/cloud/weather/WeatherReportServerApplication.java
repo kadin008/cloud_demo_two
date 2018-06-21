afb84713c1c34a1e7abb41c69bbfd303b407c8e2
kadin008 <wangchengbin_2009@vip.163.com>
2018-06-22 00:12:40 +0800

    模块3

---
+++ weather-report-server/src/main/java/cn/net/aipic/spring/cloud/weather/WeatherReportServerApplication.java
@@ -0,0 +1,12 @@
+package cn.net.aipic.spring.cloud.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherReportServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherReportServerApplication.class, args);
+    }
+}
