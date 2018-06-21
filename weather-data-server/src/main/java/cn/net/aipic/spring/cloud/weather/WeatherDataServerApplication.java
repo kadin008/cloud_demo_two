faab38eb7c38bfa38d3c636af3ae96c6b02ef7fd
kadin008 <wangchengbin_2009@vip.163.com>
2018-06-21 23:08:08 +0800

    模块2

---
+++ weather-data-server/src/main/java/cn/net/aipic/spring/cloud/weather/WeatherDataServerApplication.java
@@ -0,0 +1,12 @@
+package cn.net.aipic.spring.cloud.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherDataServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherDataServerApplication.class, args);
+    }
+}
